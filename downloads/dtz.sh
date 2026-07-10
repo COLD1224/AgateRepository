13eeeba3dd7e39537ef3950bcca38c43f993c11f
Azure <myamazonenter@gmail.com>
2026-07-10 13:09:46 -0600

    Initial Agate Website

---
+++ downloads/dtz.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "installing devtoolz 1.0"
+
+mkdir -p ~/.local/bin/
+cp "$0" ~/.local/bin/DevToolz
+chmod +x  ~/.local/bin/DevToolz
+
+echo "devtoolz installed, run DevToolz
---
+++ downloads/fsinstaller.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "FastStart! installer running" 
+ 
+mkdir  -p ~/.local/bin
+cp "$0" ~/.local/bin/fs
+chmod +x ~/.local/bin/fs
+
+echo "installed"
+ 
