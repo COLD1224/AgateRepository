89fe00f6044fbe47a80596ea2cd0479263da1fdf
COLD1224 <myamazonenter@gmail.com>
2026-07-12 14:10:19 -0600

    added useless file (used to test while true do loop) addded agate, and added azureland

---
+++ TERMLOCKERDONTUSE.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "this command will spam your terminal. please press ctrl c to exit if you arent sure"
+sleep 15
+while true
+do
+  echo "you didnt leave, you still can though. please use ctrl c"
+done
